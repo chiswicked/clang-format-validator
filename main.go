6e9e40e2213295fb943e16e51e9aa285ddc2f4c5
Norbert Metz <mr.norbert.metz@googlemail.com>
2018-12-04 20:21:16 +0000

    Format *.proto if clang-format available

--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 )
 
 func main() {
-	out, err := exec.Command("clang-format", "--help").Output()
+	if !isCommandAvailable("clang-format") {
+		fmt.Fprintln(os.Stderr, "clang-format missing")
+		os.Exit(1)
+	}
+
+	out, err := exec.Command("/bin/bash", "-c", "clang-format -style=google -i *.proto").Output()
 	if err != nil {
 		fmt.Printf("%s", err)
 	}
 	fmt.Printf("%s", out)
+}
+
+func isCommandAvailable(name string) bool {
+	cmd := exec.Command("/bin/bash", "-c", "command -v "+name)
+	if err := cmd.Run(); err != nil {
+		return false
+	}
+	return true
 }
