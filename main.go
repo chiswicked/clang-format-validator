0303f18c5d298e0b634fbd932e6b3570c748b584
Norbert Metz <mr.norbert.metz@googlemail.com>
2018-12-04 21:36:58 +0000

    Formatting proto files recursively

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 )
 
 func main() {
@@ -11,12 +12,21 @@ func main() {
 		fmt.Fprintln(os.Stderr, "clang-format missing")
 		os.Exit(1)
 	}
-
-	out, err := exec.Command("/bin/bash", "-c", "clang-format -style=google -i *.proto").Output()
+	protos, err := listFilesWithExt(".", ".proto")
 	if err != nil {
-		fmt.Printf("%s", err)
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
-	fmt.Printf("%s", out)
+
+	for _, p := range protos {
+		fmt.Println(p)
+		out, err := exec.Command("/bin/bash", "-c", "clang-format -style=google -i "+p).Output()
+		if err != nil {
+			fmt.Printf("%s", err)
+		}
+		fmt.Printf("%s", out)
+	}
+	fmt.Println(protos)
 }
 
 func isCommandAvailable(name string) bool {
@@ -25,4 +35,24 @@ func isCommandAvailable(name string) bool {
 		return false
 	}
 	return true
+}
+
+func listFilesWithExt(root string, ext string) ([]string, error) {
+	var files []string
+	err := filepath.Walk(".",
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if !info.IsDir() {
+				if filepath.Ext(path) == ext {
+					files = append(files, path)
+				}
+			}
+			return nil
+		})
+	if err != nil {
+		return []string{}, err
+	}
+	return files, nil
 }
